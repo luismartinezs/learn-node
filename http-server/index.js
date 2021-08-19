02fe39aaac17dc27e889113e22655f794a2a9e71
luismartinezs <martinez.s.luis@gmail.com>
2021-08-19 15:04:07 +0200

    mongoose

--- http-server/index.js
+++ http-server/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end('<h`>Hello, World!</h1>')
+  res.end('<h1>Hello, World!</h1>')
 })
 
 server.listen(port, () => {

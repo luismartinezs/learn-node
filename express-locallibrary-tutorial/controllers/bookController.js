e455bd69bb6f4d4eef10e3c0a0c0df67f278d84a
luismartinezs <martinez.s.luis@gmail.com>
2021-08-29 00:57:47 +0200

    Get delete books

--- express-locallibrary-tutorial/controllers/bookController.js
+++ express-locallibrary-tutorial/controllers/bookController.js
@@ -34,7 +34,6 @@ exports.index = function (req, res) {
     }
   );
 };
-// Display list of all books.
 // Display list of all Books.
 exports.book_list = function (req, res, next) {
   Book.find({}, "title author")
@@ -195,8 +194,35 @@ exports.book_create_post = [
 ];
 
 // Display book delete form on GET.
-exports.book_delete_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Book delete GET");
+exports.book_delete_get = function (req, res, next) {
+  async.parallel(
+    {
+      book: function (callback) {
+        Book.findById(req.params.id)
+          .populate("genre")
+          .populate("author")
+          .exec(callback);
+      },
+      book_instances: function (callback) {
+        BookInstance.find({ book: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.book == null) {
+        // No results.
+        res.redirect("/catalog/books");
+      }
+      // Successful, so render.
+      res.render("book_delete", {
+        title: "Delete Book",
+        book: results.book,
+        book_instances: results.book_instances,
+      });
+    }
+  );
 };
 
 // Handle book delete on POST.

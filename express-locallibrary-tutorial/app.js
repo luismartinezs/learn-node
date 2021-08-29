9e7f43fa63a56fdd4d6d73e5c1a2bca30069a8ab
luismartinezs <martinez.s.luis@gmail.com>
2021-08-29 12:42:41 +0200

    Set up production

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -16,7 +16,12 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var catalogRouter = require("./routes/catalog");
 
+var compression = require("compression");
+var helmet = require("helmet");
+
 var app = express();
+
+app.use(helmet());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -26,6 +31,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
